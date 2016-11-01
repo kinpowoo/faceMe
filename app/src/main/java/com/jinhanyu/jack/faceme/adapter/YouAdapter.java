6c84b0d2fb956d977e07adba920184bc6e5b2354
whu-iss-jack <1565771886@qq.com>
2016-11-01 11:14:34 +0800

    关注页

--- app/src/main/java/com/jinhanyu/jack/faceme/adapter/YouAdapter.java
+++ app/src/main/java/com/jinhanyu/jack/faceme/adapter/YouAdapter.java
@@ -1,12 +1,25 @@
 package com.jinhanyu.jack.faceme.adapter;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import com.jinhanyu.jack.faceme.entity.Favorite;
+
+import java.util.List;
+
 /**
  * Created by anzhuo on 2016/10/26.
  */
-public class YouAdapter extends CommonAdapter{
+public class YouAdapter extends CommonAdapter<Favorite>{
 
+    public YouAdapter(List<Favorite> data, Context context) {
+        super(data, context);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
 }
