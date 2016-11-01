b171d19b0e314d666281aa1c84e033f0dfc0f95a
whu-iss-jack <1565771886@qq.com>
2016-11-01 11:12:42 +0800

    关注页

---
+++ app/src/main/java/com/jinhanyu/jack/faceme/adapter/FollowingAdapter.java
@@ -0,0 +1,7 @@
+package com.jinhanyu.jack.faceme.adapter;
+
+/**
+ * Created by anzhuo on 2016/10/26.
+ */
+public class FollowingAdapter {
+}
---
+++ app/src/main/java/com/jinhanyu/jack/faceme/adapter/YouAdapter.java
@@ -0,0 +1,12 @@
+package com.jinhanyu.jack.faceme.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by anzhuo on 2016/10/26.
+ */
+public class YouAdapter extends CommonAdapter{
+
+}
