dbd7e1ba5d14162a06a6dd2ce9aa95fe205c17d9
kinpowoo <kinpowoo@outlook.com>
2017-01-13 22:23:33 +0800

    对标签结果进行分页加载

--- app/src/main/java/com/jinhanyu/jack/faceme/ui/SearchResultActivity.java
+++ app/src/main/java/com/jinhanyu/jack/faceme/ui/SearchResultActivity.java
@@ -6,9 +6,11 @@ import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jinhanyu.jack.faceme.LocationUtils;
 import com.jinhanyu.jack.faceme.Ptr_refresh;
@@ -19,10 +21,14 @@ import com.jinhanyu.jack.faceme.entity.Position;
 import com.jinhanyu.jack.faceme.entity.Status;
 import com.jinhanyu.jack.faceme.entity.User;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.datatype.BmobDate;
 import cn.bmob.v3.datatype.BmobQueryResult;
 import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.FindListener;
@@ -33,7 +39,10 @@ import in.srain.cube.views.ptr.PtrFrameLayout;
 /**
  * Created by anzhuo on 2016/10/28.陈礼
  */
-public class SearchResultActivity extends Activity implements View.OnClickListener{
+public class SearchResultActivity extends Activity implements View.OnClickListener,AbsListView.OnScrollListener{
+    private final int SKIPNUM=20;
+    private int count=0;
+    private int refreshCount=0;
     private ImageView iv_back;//返回
     private TextView tv_icon;//搜索的标题
     private in.srain.cube.views.ptr.PtrFrameLayout iv_ptrFrame;//下拉刷新
@@ -42,32 +51,15 @@ public class SearchResultActivity extends Activity implements View.OnClickListen
     private List<Status> list = new ArrayList<>();
     private GridViewAdapter adapter;
     private Position location;
+    private String tagIncoming,lastFetchDate;
+
     private Handler handler=new Handler(){
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
             if(msg.what==0){
                 location= (Position) msg.obj;
-                String bql="select * from Status where location near ["+
-                        location.getLongitude()+","+location.getLatitude()+"] order by createdAt";
-                BmobQuery<Status> query=new BmobQuery<Status>();
-                query.doSQLQuery(bql, new SQLQueryListener<Status>() {
-                    @Override
-                    public void done(BmobQueryResult<Status> result, BmobException e) {
-                        if(e ==null){
-                            List<Status> data = (List<Status>) result.getResults();
-                            if(data!=null && data.size()>0){
-                                list.clear();
-                                list.addAll(data);
-                                adapter.notifyDataSetChanged();
-                            }else{
-                                Log.i("smile", "查询成功，无数据返回");
-                            }
-                        }else{
-                            Log.i("smile", "错误码："+e.getErrorCode()+"，错误描述："+e.getMessage());
-                        }
-                    }
-                });
+                loadDataGeo(location);
             }
         }
     };
@@ -76,6 +68,7 @@ public class SearchResultActivity extends Activity implements View.OnClickListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_result);
 
+        tagIncoming=getIntent().getStringExtra("search_text");
         //控件初始化
         iv_back = (ImageView) findViewById(R.id.iv_back);
         tv_icon = (TextView) findViewById(R.id.tv_icon);
@@ -87,22 +80,12 @@ public class SearchResultActivity extends Activity implements View.OnClickListen
 
         //加载图片
 
-        switch (getIntent().getStringExtra("search_text")){
+        switch (tagIncoming){
             case "附近":
                 new LocationUtils(this,handler).guide();
                 break;
             default:
-                BmobQuery<Status> statusBmobQuery = new BmobQuery<>();
-                statusBmobQuery.addWhereEqualTo("tags",getIntent().getStringExtra("search_text"));
-                statusBmobQuery.include("author");
-                statusBmobQuery.findObjects(new FindListener<Status>() {
-                    @Override
-                    public void done(List<Status> data, BmobException e) {
-                        list.clear();
-                        list.addAll(data);
-                        adapter.notifyDataSetChanged();
-                    }
-                });
+                loadDataNormal();
                 break;
         }
 
@@ -112,7 +95,7 @@ public class SearchResultActivity extends Activity implements View.OnClickListen
         iv_back.setOnClickListener(this);
 
         //设置标题
-        tv_icon.setText(getIntent().getStringExtra("search_text"));
+        tv_icon.setText(tagIncoming);
 
         //下拉刷新，从新加载内容
         iv_ptrFrame.setHeaderView(refresh);
@@ -123,13 +106,10 @@ public class SearchResultActivity extends Activity implements View.OnClickListen
                 iv_ptrFrame.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-
-                        adapter.notifyDataSetChanged();
-
-
+                        refreshData();
                         iv_ptrFrame.refreshComplete();
                     }
-                }, 2000);
+                }, 1000);
             }
         });
     }
@@ -144,4 +124,127 @@ public class SearchResultActivity extends Activity implements View.OnClickListen
                 break;
         }
     }
+
+    public void loadDataNormal(){
+        BmobQuery<Status> statusBmobQuery = new BmobQuery<>();
+        statusBmobQuery.addWhereEqualTo("tags",tagIncoming);
+        statusBmobQuery.include("author");
+        statusBmobQuery.order("-createdAt");
+        statusBmobQuery.setSkip(SKIPNUM*count+refreshCount);
+        statusBmobQuery.setLimit(10);
+        statusBmobQuery.findObjects(new FindListener<Status>() {
+            @Override
+            public void done(List<Status> data, BmobException e) {
+                if(data.size()>0 && e==null) {
+                    lastFetchDate=data.get(0).getCreatedAt();
+                    list.addAll(data);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+    }
+
+    public void loadDataGeo(Position loc){
+        String bql="select * from Status where location near ["+
+        loc.getLongitude()+","+loc.getLatitude()+"] order by -createdAt skip "+
+                (SKIPNUM*count+refreshCount)+" limit 10";
+        BmobQuery<Status> query=new BmobQuery<Status>();
+        query.doSQLQuery(bql, new SQLQueryListener<Status>() {
+            @Override
+            public void done(BmobQueryResult<Status> result, BmobException e) {
+                if(e ==null){
+                    List<Status> data = (List<Status>) result.getResults();
+                    if(data!=null && data.size()>0){
+                        lastFetchDate=data.get(0).getCreatedAt();
+                        list.addAll(data);
+                        adapter.notifyDataSetChanged();
+                    }else{
+                        Log.i("smile", "查询成功，无数据返回");
+                    }
+                }else{
+                    Log.i("smile", "错误码："+e.getErrorCode()+"，错误描述："+e.getMessage());
+                }
+            }
+        });
+    }
+
+
+    public void refreshData(){
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date= null;
+        try {
+            date = sdf.parse(lastFetchDate);
+            date.setTime(date.getTime()+1000);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        switch (tagIncoming){
+            case "附近":
+        String bql="select * from Status where location near ["+
+                location.getLongitude()+","+location.getLatitude()+"] and createdAt > date("+lastFetchDate+") order by -createdAt";
+        BmobQuery<Status> query=new BmobQuery<Status>();
+        query.doSQLQuery(bql, new SQLQueryListener<Status>() {
+            public void done(BmobQueryResult<Status> result, BmobException e) {
+                if(e ==null){
+                    List<Status> data = (List<Status>) result.getResults();
+                    if(data!=null && data.size()>0){
+                        lastFetchDate=data.get(0).getCreatedAt();
+                        list.addAll(0,data);
+                        adapter.notifyDataSetChanged();
+                    }else{
+                       Toast.makeText(SearchResultActivity.this,"刷新失败",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+                break;
+            default:
+        BmobQuery<Status> statusBmobQuery = new BmobQuery<>();
+        statusBmobQuery.addWhereEqualTo("tags",tagIncoming);
+        statusBmobQuery.include("author");
+        statusBmobQuery.order("-createdAt");
+        statusBmobQuery.addWhereGreaterThan("createdAt",new BmobDate(date));
+        statusBmobQuery.findObjects(new FindListener<Status>() {
+            @Override
+            public void done(List<Status> data, BmobException e) {
+                if(data.size()>0&&e==null){
+                    lastFetchDate=data.get(0).getCreatedAt();
+                    refreshCount+=data.size();
+                    list.addAll(0,data);
+                    adapter.notifyDataSetChanged();
+                }else{
+                    Toast.makeText(SearchResultActivity.this,"刷新失败",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+                break;
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        finish();
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView absListView, int i) {
+        if(i==SCROLL_STATE_FLING) {
+            if (absListView.getLastVisiblePosition() == absListView.getCount() - 1){
+              switch (tagIncoming){
+                  case "附近":
+                      loadDataGeo(location);
+                      break;
+                  default:
+                      loadDataNormal();
+                      break;
+              }
+        }
+    }
+    }
+
+    @Override
+    public void onScroll(AbsListView absListView, int i, int i1, int i2) {
+
+    }
 }
+
